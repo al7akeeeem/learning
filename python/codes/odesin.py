b0efd0add1fb5ef74bb92f861fa735f200b63080
Ahmad Al Saadi <saadias@kw-10423.kaust.edu.sa>
2014-11-11 11:10:58 +0300

    new file is added while ODE.py is slightly modified

--- python/codes/odesin.py
+++ python/codes/odesin.py
@@ -2,14 +2,13 @@ import numpy as np
 from  scipy.integrate import odeint 
 import matplotlib.pyplot as plt # for plotting commands
 def deriv(y,t):  # return derivatives of the array y
-    a = -2.0
-    b = -0.1
-    return np.array([ y[1], a*y[0]+b*y[1] ])
+    return np.array([ y[1],np.sin(t)])
 time =np.linspace(0.0,10.0,1000)
-yinit =np.array([0.0005,0.2])  # initial values
+yinit =np.array([0.0,-1.0])  # initial values
 y = odeint(deriv,yinit,time)
 plt.plot(time,y[:,0],label='y')  # y[:,0] is the first column of y
 plt.plot(time,y[:,1],label="y'")
+plt.plot(time,-1*np.cos(time),"--",label="exact sol.")
 plt.xlabel('$t$')
 plt.ylabel('$y$')
 plt.legend()
